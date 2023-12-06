a2cb10a62442059b6bbd801adefd014f801d2d46
Kamalraj18 <106262986+Kamalraj18@users.noreply.github.com>
2023-12-06 11:47:50 +0530

    Delete UDEMY directory

--- UDEMY/CORE JAVA/QUIZ/src/App.java
+++
@@ -1,7 +0,0 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-        QuizService s= new QuizService();
-    
-        s.showdetails();
-    }
-}
--- UDEMY/CORE JAVA/demorest/src/main/java/com/example/rest/demorest/studentrepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.rest.demorest;
-import java.util.*;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface studentrepository extends JpaRepository<studententity,Integer> {
-
-    List<studententity> findBysname(String sname);
-    
-}
