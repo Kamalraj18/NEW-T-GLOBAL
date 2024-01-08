1582606c59bfd4791de954f961c13a64d923ae46
Kamalraj18 <106262986+Kamalraj18@users.noreply.github.com>
2024-01-08 23:22:08 +0530

    Delete journal.py

--- journal.py
+++
@@ -1,7 +0,0 @@
-date = input("date: ")
-rating = input()
-thoughts = input("let me know your thoughts: ")
-
-with open(f"{date}", "w") as file:
-    file.write(rating + 1 * "\n")
-    file.write(thoughts)
